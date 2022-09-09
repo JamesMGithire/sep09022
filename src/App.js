016add5a029123c37e41673c7355b460c9bd4520
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-09 15:16:16 +0300

    updating state from from changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [form, setForm] = React.useState({
+    email: "",
+    password1: "",
+    password2: "",
+    join: false
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const { value, name } = event.target;
+  }
+  function handleChange(event) {
+    const { type, value ,name, checked } = event.target;
+    console.log(checked)
+    setForm(prevVal => { return { ...prevVal, [name]: type === "checkbox"? checked : value } });
+  }
+  console.log(form)
+
   return (
     <div className="App">
       <header className="App-header bgpurple">
-        <div className='width300'>
+        <form
+          onSubmit={handleSubmit}
+          className='width300'
+        >
           <input
             type="text"
+            name='email'
+            onChange={handleChange}
             placeholder='johndoe@example.com'
           />
           <input
             type="text"
+            name='password1'
+            onChange={handleChange}
             placeholder='Password'
           />
           <input
             type="text"
+            name='password2'
+            onChange={handleChange}
             placeholder='Confirm Password'
           />
           <div>
-            <input type='checkbox' className='width10' />
+            <input
+              type='checkbox'
+              checked={form.join}
+              className='width10'
+              name='join'
+              onChange={handleChange}
+            />
             <label>I want to join the newsletter</label>
           </div>
           <button
             className='bgpurple color-white width60'
-            type='submit'>
+            type='submit'
+          >
             Sign up
           </button>
-        </div>
+        </form>
       </header>
     </div>
   );
